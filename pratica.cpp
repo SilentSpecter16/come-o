3b39a4adbf583593c7d6e020617b289df08a277c
SilentSpecter16 <vmoneyxxx@gmail.com>
2025-01-03 18:14:17 -0300

    so praticando
    c++

--- pratica.cpp
+++ pratica.cpp
@@ -1,9 +1,103 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
-	int x, y = 0,m,es;
+	int a = 0;
+	string lop;
+
+
+
+	do {
+		cout << "Digite 1 ou + para soma\nDigite 2 ou - para subtracao\nDigite 3 ou * para multiplicacao\nDigite 4 ou / para divisao\nDigite 5 ou X para sair\n\n";
+
+
+
+
+
+
+		cout << "Digite a operacao: ";
+		cin >> lop;
+		cout << endl;
+		if (lop == "+" or lop == "1") {
+			a = 1;
+		}
+		else if (lop == "-" or lop == "2") {
+			a = 2;
+		}
+		else if (lop == "*" or lop == "3") {
+			a = 3;
+		}
+		else if (lop == "/" or lop == "4") {
+			a = 4;
+		}
+		else if (lop == "X" or lop == "x" or lop == "5") {
+			a = 5;
+		}
+
+		if (a == 1) {
+			int x, y, z;
+			cout << "Digite o primeiro numero: ";
+			cin >> x;
+			cout << "Digite o segundo numero: ";
+			cin >> y;
+			z = x + y;
+			cout << "\nA soma dos numeros e: " << z << "\n\n";
+		}
+		else if (a == 2) {
+			int x, y, z;
+			cout << "Digite o primeiro numero: ";
+			cin >> x;
+			cout << "Digite o segundo numero: ";
+			cin >> y;
+			z = x - y;
+			cout << "\nA subtracao dos numeros e: " << z << "\n\n";
+		}
+		else if (a == 3) {
+			int x, y, z;
+			cout << "Digite o primeiro numero: ";
+			cin >> x;
+			cout << "Digite o segundo numero: ";
+			cin >> y;
+			z = x * y;
+			cout << "\nA multiplicacao dos numeros e: " << z << "\n\n";
+		}
+		else if (a == 4) {
+			int x, y, z;
+			cout << "Digite o primeiro numero: ";
+			cin >> x;
+			cout << "Digite o segundo numero: ";
+			cin >> y;
+			if (y == 0) {
+				cout << "Nao poder dividir por 0\n";
+			}
+			else {
+				z = x / y;
+				cout << "\nA divisao dos numeros e: " << z << "\n\n";
+			}
+		}
+		else if (a == 5) {
+			cout << "Saindo...\n";
+		}
+		else {
+			cout << "Operacao invalida\n";
+			cout <<endl<< a << endl;
+		}
+
+	} while (a != 5);
+	
+	
+	
+	return 0;
+}
+
+
+/*
+int main() {
+	int x, y = 0, m, es;
+
+
 	do{
 		cout << "\nDigite o numero da tabuada: ";
 		cin >> x;
@@ -24,7 +118,6 @@ int main() {
 
 	} while (es != 1);
 	
-	
-	
 	return 0;
 }
+*/
